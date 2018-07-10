0afabf496ffe5640d187e30581aaec777fac8435
thechucklingatom <thechucklingatom@gmail.com>
2018-07-10 16:27:12 -0600

    And this computer should be done.

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -3,5 +3,6 @@ import { Article } from './article';
 export const ARTICLES: Article[] = [
     { name: 'article 1', body: 'this is an article', searchTags: [], createdDate: new Date('2018-01-01')},
     { name: 'article 2', body: 'this is also an article', searchTags: [], createdDate: new Date('2018-01-02')},
-    { name: 'article 3', body: 'this is a third article', searchTags: [], createdDate: new Date('2018-01-03')}
+    { name: 'article 3', body: 'this is a third article', searchTags: [], createdDate: new Date('2018-01-03')},
+    { name: 'article 4', body: 'this is a third article', searchTags: [], createdDate: new Date('2018-01-04')}
 ];
